ce259ec98a1cb6495fd8445eb4b2eb357455b6d7
Ali Bagery <loghman.ali1382@gmail.com>
2024-06-20 21:52:38 +0330

    sabt3

--- final.project2/run1.py
+++ final.project2/run1.py
@@ -1,6 +1,10 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
 import requests
+import aiohttp
+
+
+headers = {'Authorization': f'Token {token}'}
 
 async def start_command(message: types.Message):
     await message.reply("خوش آمدید")
@@ -43,13 +47,16 @@ async def tamam_arz(message: types.Message):
 async def option_2_command(message: types.Message):
     
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Sub Option 1"), types.KeyboardButton("Sub Option 2"))
-    keyboard.add(types.KeyboardButton("Sub Option 3"), types.KeyboardButton("Sub Option 4"), types.KeyboardButton("Sub Option 5"))
+    keyboard.add(types.KeyboardButton("پروفایل کاربر"), types.KeyboardButton("کیف پول"))
+    keyboard.add(types.KeyboardButton("لیست تراکنش ها"), types.KeyboardButton("لیست واریزی ها"))
+    keyboard.add(types.KeyboardButton("بازار های مورد علاقه"))
     keyboard.add(types.KeyboardButton("Back to Main Menu"))
     await message.reply("لطفا یک گزینه را انتخاب کنید", reply_markup=keyboard)
 
 
 
+
+#منو تمامی ارز ها
 async def usdt_tamam(message: types.Message):
     response = requests.get('https://api.nobitex.ir/market/stats?srcCurrency=btc&dstCurrency=usdt')
     data = response.json()['global']['binance']
@@ -61,7 +68,7 @@ async def usdt_tamam(message: types.Message):
 
 
 async def rls_tamam(message: types.Message):
-    response = requests.get('https://api.nobitex.ir/market/stats?srcCurrency=btc&dstCurrency=usdt')
+    response = requests.get('https://api.nobitex.ir/market/stats?srcCurrency=btc&dstCurrency=rls')
     data = response.json()['global']['binance']
     binance_data_str = str(data)
     
@@ -70,11 +77,37 @@ async def rls_tamam(message: types.Message):
         await message.reply(binance_data_str[i:i + chunk_size])
     
 
+#پروفایل کاربر
+
+
+async def user_profile(message: types.Message):
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.nobitex.ir/users/profile', headers=headers) as response:
+            data = await response.json()
+            await message.reply(data)
+#منو کیف پول
+
+async def tamam_arz(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(types.KeyboardButton("دریافت لیست کیف پول ها"))
+    keyboard.add(types.KeyboardButton("نمایش موجودی هر کیف پول"))
+    keyboard.add(types.KeyboardButton("نمایش کل موجودی"))
+    keyboard.add(types.KeyboardButton("Back to Menu "))
+    await message.reply("لطفا یک گزینه را انتخاب کنید", reply_markup=keyboard)
+
+
+
+#کیف پول کاربر
+
+async def list_kif(message: types.Message):
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.nobitex.ir/users/wallets/list', headers=headers) as response:
+            data = await response.json()
+            await message.reply(data)
+
 
 async def main():
     bot = Bot(token="6819049569:AAHdaEjILbHFL7EvMBXh64Io1FAFfLrKC2U", proxy="http://10.111.222.1:8080/")
-
-
     dp = Dispatcher(bot)
 
     
@@ -88,7 +121,11 @@ async def main():
     dp.register_message_handler(option_1_command, text="Back to Menu 1")
     dp.register_message_handler(tamam_arz,text="نمایش قیمت تمامی ارز‌های موجود")
     dp.register_message_handler(usdt_tamam,text="به دلار")
-    dp.register_message_handler()
+    dp.register_message_handler(rls_tamam,text="به ریال")
+
+    dp.register_message_handler(user_profile,text="پروفایل کاربر")
+    dp.register_message_handler(list_kif,text="لیست کیف پول ها")
+
 
     
     await dp.start_polling()
