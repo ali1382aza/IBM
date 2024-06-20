a25339884f0823aa6b1c42723ca466e7d2e67b09
ali1382aza <157964959+ali1382aza@users.noreply.github.com>
2024-06-20 08:32:06 +0330

    Delete text.c directory

--- text.c/final_exam/1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int a=5,b=6,c=0;
-    int d=(a&&b&&c);
-    printf("%d",d);
-}
--- text.c/final_exam/4.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int upper=10,lower=5;
-    int c=++upper;
-    printf("%d %d",c,upper);
-    }
-
